c4cbc10d7e446ace3de8adfac75e3bbbdd8fbc75
Vimukthi_Randunu <wkvp.randunu@gmail.com>
2024-10-10 05:33:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './components/ui/Header';
+import './App.css';
+
+const App = () => {
+  return (
+    <Header />
+  )
+}
+
+export default App
---
+++ src/components/ui/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <Header className='center'>
+        
+    </Header>
+  )
+}
+
+export default Header
